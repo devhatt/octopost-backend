e95f39af6ddc6d342568263874d47b6093508814
frompaje <yanedwards11@gmail.com>
2024-05-08 17:47:37 -0300

    feat: addicionei repeatPassword no controller,model,services

--- src/features/user/services/user-create-service.ts
+++ src/features/user/services/user-create-service.ts
@@ -1,11 +1,23 @@
 import type { UserCreateModel } from '../models/user-create-model.js';
 import type { UserRepository } from '../repositories/user-repository/user-repository.js';
+import { ConflictError } from '@/shared/errors/conflict-error.js';
 import type { Service } from '@/shared/protocols/service.js';
 
 export class UserCreateService implements Service<UserCreateModel> {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute({ email, name, password, username }: UserCreateModel) {
+  async execute({
+    email,
+    name,
+    password,
+    repeatPassword,
+    username,
+  }: UserCreateModel) {
+    if (password != repeatPassword) {
+      throw new ConflictError(
+        'The password and the repetition of the password dont match. '
+      );
+    }
     await this.userRepository.create({
       email,
       name,
