988a0d37f0ad80f93b83cb03bc4142d92fa76f60
Thiago <devt.calazans@gmail.com>
2024-05-08 19:51:52 -0300

    refactor: user mock and user test for adapt

--- src/shared/test-helpers/mocks/user.mock.ts
+++ src/shared/test-helpers/mocks/user.mock.ts
@@ -4,13 +4,10 @@ import type { User } from '@prisma/client';
 export class UserMock {
   public static create() {
     return {
-      createdAt: new Date(),
-      deletedAt: null,
       email: faker.internet.email(),
       id: faker.string.numeric(),
       name: faker.person.firstName(),
       password: faker.internet.password(),
-      updatedAt: new Date(),
       username: faker.internet.userName(),
     };
   }
