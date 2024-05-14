5d78b839a20bc5f1cf4ca06e08e393673baf60d8
Clerivaldo <clerivaldoj@gmail.com>
2024-05-13 22:33:51 -0300

    feat: add error treatment to @unique violation on user creation

--- src/features/user/repositories/user-repository/user-repository.ts
+++ src/features/user/repositories/user-repository/user-repository.ts
@@ -1,18 +1,31 @@
 import type { Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/react-native.js';
 import { database } from '@/shared/infra/database/database.js';
+import { ConflictError } from '@/shared/errors/conflict-error.js';
 
 type CreateUserParams = Prisma.Args<typeof database.user, 'create'>['data'];
 
 export class UserRepository {
   async create({ email, name, password, username }: CreateUserParams) {
-    return database.user.create({
-      data: {
-        email,
-        name,
-        password,
-        username,
-      },
-    });
+    try {
+      return database.user.create({
+        data: {
+          email,
+          name,
+          password,
+          username,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ConflictError(
+          'There is already a user with this email or username'
+        );
+      throw error;
+    }
   }
 
   async findById(id: string) {
