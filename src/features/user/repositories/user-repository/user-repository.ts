041df4def020d98052c46270c55946afa0791e68
Clerivaldo <clerivaldoj@gmail.com>
2024-05-16 11:06:36 -0300

    refactor: removed error check from repository

--- src/features/user/repositories/user-repository/user-repository.ts
+++ src/features/user/repositories/user-repository/user-repository.ts
@@ -1,31 +1,18 @@
 import type { Prisma } from '@prisma/client';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/react-native.js';
 import { database } from '@/shared/infra/database/database.js';
-import { ConflictError } from '@/shared/errors/conflict-error.js';
 
 type CreateUserParams = Prisma.Args<typeof database.user, 'create'>['data'];
 
 export class UserRepository {
   async create({ email, name, password, username }: CreateUserParams) {
-    try {
-      return database.user.create({
-        data: {
-          email,
-          name,
-          password,
-          username,
-        },
-      });
-    } catch (error) {
-      if (
-        error instanceof PrismaClientKnownRequestError &&
-        error.code === 'P2002'
-      )
-        throw new ConflictError(
-          'There is already a user with this email or username'
-        );
-      throw error;
-    }
+    return database.user.create({
+      data: {
+        email,
+        name,
+        password,
+        username,
+      },
+    });
   }
 
   async findById(id: string) {
