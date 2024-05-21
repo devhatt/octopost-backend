3ee34fa550ae7a833c9ecb24b9f4c91bb5df89de
Edwards Yan <117830579+Frompaje@users.noreply.github.com>
2024-05-20 21:33:34 -0300

    [Usuário][Cadastro] - Expor endpoint para cadastro de usuário  (#64)
    * docs: documentation of error 409 and readjustment of the find-user-by-id file
    
    * feat: addicionei repeatPassword no controller,model,services
    
    * test: adjust the tests by adding repeatPassword
    
    * refactor: "Correcting the response error from 409 to 400"

--- src/features/user/routes/user-routes.ts
+++ src/features/user/routes/user-routes.ts
@@ -8,6 +8,7 @@ const router = Router();
 const { userController } = userControllerFactory();
 router.post('/create', userController.create);
 router.get('/:id', userController.userFindById);
+router.post('/', userController.create);
 
 export default {
   prefix: 'users',
