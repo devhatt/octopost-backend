daf3571462e723c83c62c771c08cb30ca844aefe
Kevin Gama <kevin.bg@outlook.com>
2024-06-04 14:39:13 -0300

    fix: remove duplicated endpoint to create a new user

--- src/features/user/routes/user-routes.ts
+++ src/features/user/routes/user-routes.ts
@@ -6,7 +6,6 @@ import { userControllerFactory } from './user-controller-factory.js';
 const router = Router();
 
 const { userController } = userControllerFactory();
-router.post('/', userController.create);
 router.get('/:id', userController.userFindById);
 router.post('/', userController.create);
 
