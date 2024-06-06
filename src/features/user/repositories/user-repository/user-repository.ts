c9265876512ed248067e9b8b42e5e9da8a825c96
Thiago Calazans <98334306+thiagoCalazans-dev@users.noreply.github.com>
2024-06-06 08:57:02 -0300

    feat: repository to update isActive status (#112)
    * feat: repository to update isActive status
    
    * fix: repository param
    
    * fix: add migration

--- src/features/user/repositories/user-repository/user-repository.ts
+++ src/features/user/repositories/user-repository/user-repository.ts
@@ -30,4 +30,15 @@ export class UserRepository {
 
     return user;
   }
+
+  async updateIsActiveStatus(userId: string): Promise<void> {
+    await database.user.update({
+      data: {
+        isActive: true,
+      },
+      where: {
+        id: userId,
+      },
+    });
+  }
 }
