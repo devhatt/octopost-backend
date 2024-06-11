f41073dc6d23b7bd8543173f471cf13160828402
matheus.webp <matheus.webp@gmail.com>
2024-06-11 13:28:03 -0300

    chore: remove two migrations as they are no longer needed

--- prisma/migrations/20240605013537_add_deleted_at_to_account_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "accounts" ADD COLUMN     "deleted_at" TIMESTAMP(3);
--- prisma/migrations/20240609195827_remove_deleted_at_field_from_account_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `deleted_at` on the `accounts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "accounts" DROP COLUMN "deleted_at";
