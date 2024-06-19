123b1f2460a81b8cb55c4110e1b2a4aeecfc8d0e
Thiago Calazans <98334306+thiagoCalazans-dev@users.noreply.github.com>
2024-06-19 12:21:52 -0300

    Issue 34 (#124)
    * feat: auth model
    
    * feat: auth login service
    
    * feat: auth login service
    
    * fix: auth login custom error
    
    * fix: test coverage

---
+++ src/features/auth/services/auth-login-service.ts
@@ -0,0 +1,31 @@
+import type { Service } from '@/shared/protocols/service.js';
+import type { CryptoAdapter } from '@/shared/infra/crypto/crypto-adapter.js';
+import type { AuthRepository } from '@/features/authentication/repositories/auth-repository/auth-repository.js';
+import type { AuthLoginModel } from '../models/auth-login-models.js';
+import type { JWTHelper } from '@/shared/infra/jwt/jwt.js';
+import { InvalidCredentialsError } from '@/shared/errors/invalid-credentials-error.js';
+
+export class AuthLoginService implements Service<AuthLoginModel> {
+  constructor(
+    private readonly authRepository: AuthRepository,
+    private readonly crypto: CryptoAdapter,
+    private jwt: JWTHelper
+  ) {}
+
+  async execute({ password, username }: AuthLoginModel) {
+    const hashedPassword = await this.crypto.encrypt(password);
+
+    const user = await this.authRepository.findUserByCredentials({
+      password: hashedPassword,
+      username,
+    });
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const token = this.jwt.createToken({ userId: user.id });
+
+    return token;
+  }
+}
