694bc85127bf84fa3b604526266cc7495633e5c4
k1nha <lucascmpus@gmail.com>
2024-06-19 00:33:11 -0300

    feat: add pnpm script for generating Prisma client

--- __mocks__/prisma.ts
+++ __mocks__/prisma.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-default-export */
 import type { PrismaClient as PrismaClientType } from '@prisma/client';
 import { vi } from 'vitest';
 import { mockDeep } from 'vitest-mock-extended';
@@ -5,3 +6,5 @@ import { mockDeep } from 'vitest-mock-extended';
 export const prisma = mockDeep<PrismaClientType>();
 
 export const PrismaClient = vi.fn(() => prisma);
+
+export default { PrismaClient };
