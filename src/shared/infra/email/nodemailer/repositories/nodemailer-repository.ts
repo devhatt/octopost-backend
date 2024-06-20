657a3d6f6911ceed60fa725547bafbd22f6f36bd
matheus.webp <matheus.webp@gmail.com>
2024-06-20 09:57:43 -0300

    feat: add NodemailerRepository

---
+++ src/shared/infra/email/nodemailer/repositories/nodemailer-repository.ts
@@ -0,0 +1,28 @@
+import { InternalServerError } from '@/shared/errors/internal-server-error.js';
+import path from 'node:path';
+import Handlebars from 'handlebars';
+import fs from 'node:fs';
+
+export class NodemailerRepository {
+  compileTemplate(source: string, data: unknown): string {
+    try {
+      const compiledTemplate = Handlebars.compile(source);
+      return compiledTemplate(data);
+    } catch {
+      throw new InternalServerError('template compilation failed');
+    }
+  }
+
+  readTemplate(templateName: string): Promise<string> {
+    try {
+      const templatePath = path.resolve(
+        import.meta.dirname,
+        '../../templates',
+        `${templateName}.html`
+      );
+      return fs.promises.readFile(templatePath, 'utf8');
+    } catch {
+      throw new InternalServerError('template file read failed');
+    }
+  }
+}
