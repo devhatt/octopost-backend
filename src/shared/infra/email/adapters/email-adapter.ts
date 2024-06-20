0475207942efcbd7a773a47c145d2db1646bfadd
matheus.webp <matheus.webp@gmail.com>
2024-06-20 09:56:53 -0300

    refactor: update sendEmail to receive EmailModel

--- src/shared/infra/email/adapters/email-adapter.ts
+++ src/shared/infra/email/adapters/email-adapter.ts
@@ -1,3 +1,5 @@
+import type { EmailModel } from '@/shared/infra/email/models/email-model.js';
+
 export interface EmailAdapter {
-  sendEmail(template: string, recipient: string): Promise<void>;
+  sendEmail(data: EmailModel): Promise<void>;
 }
