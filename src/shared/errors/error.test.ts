9dfdb75898189fea2574c54ee69f6b21d5d174b0
matheus.webp <matheus.webp@gmail.com>
2024-06-20 09:49:49 -0300

    feat: add custom error called InternalServerError

--- src/shared/errors/error.test.ts
+++ src/shared/errors/error.test.ts
@@ -7,7 +7,7 @@ import { ValidationError } from './validation-error';
 
 describe('[Errors]', () => {
   describe('http-error', () => {
-    it('should parse to json correctly', () => {
+    it('parses to json correctly', () => {
       const error = new HttpError(200, 'error');
 
       expect(error.toJSON()).toStrictEqual({
@@ -18,7 +18,7 @@ describe('[Errors]', () => {
   });
 
   describe('validate-error', () => {
-    it('should parse to json correctly', () => {
+    it('parses to json correctly', () => {
       const error = new ValidationError('error', [
         'first validation error',
         'second validation error',
@@ -32,7 +32,7 @@ describe('[Errors]', () => {
   });
 
   describe('user-not-found-error', () => {
-    it('should parse to json correctly without additional errors', () => {
+    it('parses to json correctly without additional errors', () => {
       const error = new UserNotFound();
 
       expect(error.toJSON()).toStrictEqual({
@@ -41,7 +41,7 @@ describe('[Errors]', () => {
       });
     });
 
-    it('should parse to json correctly with additional errors', () => {
+    it('parses to json correctly with additional errors', () => {
       const additionalErrors = ['first error', 'second error'];
       const error = new UserNotFound('User not found', additionalErrors);
 
@@ -53,7 +53,7 @@ describe('[Errors]', () => {
   });
 
   describe('bad-request-error', () => {
-    it('should parse to json correctly', () => {
+    it('parses to json correctly', () => {
       const error = new BadRequestError('error message');
 
       expect(error.toJSON()).toStrictEqual({
