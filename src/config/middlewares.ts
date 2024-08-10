72ad3f5569f09b0cfeb648a89e491e7963340862
matheus-webp <matheus.webp@gmail.com>
2024-08-10 10:43:37 -0300

    feat: add authfilter to setupmiddlewares

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -1,12 +1,14 @@
 import type { Express } from 'express';
 import httpContext from 'express-http-context';
 
+import { authFilter } from '@/middlewares/auth/auth-filter';
 import { bodyParser } from '@/middlewares/body-parser/body-parser';
 import { cors } from '@/middlewares/cors/cors';
 import { httpLogger } from '@/middlewares/logger/http-logger';
 import { requestId } from '@/middlewares/requestId/request-id';
 
 export function setupMiddlewares(app: Express): void {
+  app.use(authFilter);
   app.use(bodyParser);
   app.use(cors);
   app.use(httpLogger);
