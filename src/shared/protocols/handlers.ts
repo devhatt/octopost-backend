0f1152f001c53fdfc47630c2f0fc07d43cde1ba1
matheus-webp <matheus.webp@gmail.com>
2024-08-15 11:56:52 -0300

    refactor: adapt AsyncRequestHandler

--- src/shared/protocols/handlers.ts
+++ src/shared/protocols/handlers.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 import type { NextFunction, Request, Response } from 'express';
 
 export type AsyncRequestHandler<
   Req = Request,
   Res = Response,
   Next = NextFunction,
-> = (req: Req, res: Res, next: Next) => Promise<any>;
+> = (req: Req, res: Res, next: Next) => any | Promise<any>;
