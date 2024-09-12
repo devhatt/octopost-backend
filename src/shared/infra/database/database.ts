d992c291d39187007f68b37b9d64344abe53bee6
Paulo V T Silva <pilutechinformatica@gmail.com>
2024-09-12 16:18:19 -0300

    feat: ajust mongo shemas and add integrate with prisma

--- src/shared/infra/database/database.ts
+++ src/shared/infra/database/database.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-const database = new PrismaClient();
+const database = new PrismaClient({
+  log: ['info', 'warn', 'query', 'error'],
+});
 
 export { database };
